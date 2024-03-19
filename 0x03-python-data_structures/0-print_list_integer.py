0b95a64333aca74be9359dd4146e500de16b6e6b
Denis Kinyua <kimenjudenis@gmail.com>
2024-03-19 22:28:28 +0300

    Task 0: Print integers in a list(Adding comments)

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Function that prints an integers in a list
+
 def print_list_integer(my_list=[]):
     for num in range(len(my_list)):
         print("{:d}".format(my_list[num]))
