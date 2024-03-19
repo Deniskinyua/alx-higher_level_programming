0a4e7d5ecfe8ec74da559d88d91644b744c6360d
Denis Kinyua <kimenjudenis@gmail.com>
2024-03-20 01:55:37 +0300

    Task 5: Remove characters from a list

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Write a function that removes all characters c and c from a string
+  - Should return a new string
+  - Do not import any module
+  - Do not use str.replace()
+"""
+
+
+def no_c(my_string):
+    new_string = list(my_string)
+    length_of_array = len(new_string)
+    r_string = []
+    n_string = []
+
+    for c in range(length_of_array):
+        if new_string[c] != 'c':
+            r_string.append(new_string[c])
+
+    for C in range(len(r_string)):
+        if r_string[C] != 'C':
+            n_string.append(r_string[C])
+    return (''.join(n_string))
