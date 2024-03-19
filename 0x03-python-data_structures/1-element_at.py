bef40efa0195f8b71de138b6491559e5b30e3b9b
Denis Kinyua <kimenjudenis@gmail.com>
2024-03-19 23:00:00 +0300

    Task 1: Retrieve element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,10 +3,12 @@
 # Retrieve an elements from a list like in C
 
 def element_at(my_list, idx):
+    length_of_list = len(my_list)
+
     if idx < 0:
         return(None)
 
-    if idx > len(my_list) - 1:
+    if idx > length_of_list - 1:
         return(None)
 
     return(my_lis[idx])
