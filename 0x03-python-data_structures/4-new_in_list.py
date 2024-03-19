407ec5338aac2cf0cda3e48d9ddf0dfe00e958c1
Denis Kinyua <kimenjudenis@gmail.com>
2024-03-20 00:40:26 +0300

    Task 4: Replace element at index, dont modify original(Change 1)

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -12,6 +12,7 @@ Replace an element at index idx, without modifying the original list
 def new_in_list(my_list, idx, element):
     # Creating a copy
     my_list_copy = my_list
+    my_list_length = len(my_list)
 
     if idx < 0:
         return (my_list_copy)
