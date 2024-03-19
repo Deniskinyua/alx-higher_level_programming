8d029b8c6e4f72d2e54b5d898de2ee6f22442084
Denis Kinyua <kimenjudenis@gmail.com>
2024-03-20 00:24:35 +0300

    Task 3: Print elements in a list in reverse order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Print all integers of a list, in reverse order
+    - One integer per line
+    - Do not import any module
+    - Assume that the list only cotains integers
+    - Do not cast
+    - MUST use str.format() to print
+"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    reversed_my_list = (my_list[::-1])
+    my_list_length = len(my_list)
+
+    for num in range(my_list_length):
+        print(reversed_my_list[num])
