6077e6b02323bde012a9cfeb8a312ce46d64571a
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-03 01:43:46 +0300

    Task 0 : Write a function that prints x elements of a list(2)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     elements = 0
-    for i in range(e):
+    for i in range(x):
         try:
-            print("{}".format(my_list[e]), end="")
+            print("{}".format(my_list[i]), end="")
             elements = elements + 1
         except IndexError:
             continue
