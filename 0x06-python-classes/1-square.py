13dda46a23ce27cca6d2e4105d5c1891ef51e11b
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-03 03:42:49 +0300

    Task 1: Write a class Square that defines a square by : based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a class Square that defines a square by : (based on 0-square.py)  """
+
+
+class Square:
+    """ pass rivate Instance attribute : size"""
+
+    def __init__(self, size):
+
+        """
+        Square's size
+        """
+
+        self.__size = size
