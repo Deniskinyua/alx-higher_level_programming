e5cc7be92842da1bc52ab6269d873f862af248a6
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-10 02:59:35 +0300

    Task 29

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def magic_string(strn=[]):
+    """Returns a string --> BestSchool n times the number of iteration
+    """
+    strn += ["BestSchool"]
+    return (", ".join(strn))
