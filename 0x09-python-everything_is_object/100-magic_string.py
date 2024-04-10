2cccf7b68fceaa13769168004ed6f6c615f4fb62
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-10 03:01:27 +0300

    Task Advanced Task (100-magic_string.py)

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string(strn=[]):
-    """Returns a string --> BestSchool n times the number of iteration
-    """
     strn += ["BestSchool"]
     return (", ".join(strn))
