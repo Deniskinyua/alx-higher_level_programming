7ed956f9a6a00e8574c3be8c44e911257cb4f3dd
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-16 03:33:42 +0300

    Task 6: Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Improved Geometry
+"""
+
+
+class BaseGeometry:
+    """public instance method"""
+    def area(self):
+        """raises exception with message"""
+        raise Exception("area() is not implemented")
