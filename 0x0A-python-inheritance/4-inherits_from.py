7e1d03f5d227cb9751c77cfdaa2cfc0d697708e6
Denis Kinyua <kimenjudenis@gmail.com>
2024-04-16 03:28:10 +0300

    Task 4: Only subclass of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@ Only subclass of
 
 
 def inherits_from(obj, a_class):
-    """obj of instance of a class inherited from the specified class? --> true`"""
+    """obj of instance inherited from the specified class? --> true`"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
