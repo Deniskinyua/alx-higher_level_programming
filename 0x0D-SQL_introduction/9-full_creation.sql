cf8a34eb9d752a17725b2291764f36cef6a4a0c3
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 01:19:26 +0300

    Task 9 : Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,7 +8,7 @@
 -- etc..
 CREATE TABLE IF NOT EXISTS second_table (ID INT, NAME VARCHAR(256), SCORE INT);
 INSERT INTO second_table (ID, NAME, SCORE) VALUES(1, "John", 10);
-INSERT INTO second_table (ID, NAME, SCORE) VALUES(3, "Alex", 3);
+INSERT INTO second_table (ID, NAME, SCORE) VALUES(2, "Alex", 3);
 INSERT INTO second_table (ID, NAME, SCORE) VALUES(3, "Bob", 14);
 INSERT INTO second_table (ID, NAME, SCORE) VALUES(4, "George", 8);
 
