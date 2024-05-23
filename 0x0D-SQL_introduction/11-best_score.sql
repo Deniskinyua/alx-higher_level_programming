73da6ab1a5437af4ca049c62e1c75c7eb9a6d334
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 01:28:34 +0300

    Task 11: Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- List all records witha score >= 10 from second_table
+-- Display: score name
+-- Ordered by score (Top first)
+-- DB provided as args
+SELECT SCORE, NAME FROM second_table WHERE SCORE >= 10 ORDER BY SCORE DESC;
