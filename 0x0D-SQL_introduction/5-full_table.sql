d4508b42c598678d8c60062e99b93ed4d5cb91c1
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 00:49:11 +0300

    Task 5 : Show full desc of a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Prints full desc of the table first_table from the hbtn_0c_0 database
+-- Database name will be passed as an argument
+-- DO NOT use DESCRIBE or EXPLAIN
+SHOW CREATE TABLE `first_table`;
