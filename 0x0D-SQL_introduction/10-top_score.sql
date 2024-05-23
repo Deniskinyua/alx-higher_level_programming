d3ef6de6d50888469da0b345a185efde66c723d7
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 01:23:54 +0300

    Task 10 : List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of second_table
+-- Display score & name in this order
+-- Ordered by score (Top first)
+-- DB passed as args
+SELECT SCORE, NAME FROM second_table ORDER BY SCORE DESC;
