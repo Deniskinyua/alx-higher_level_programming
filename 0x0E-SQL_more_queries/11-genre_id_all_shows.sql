c10bc51a069acd22b7e7005ae50eaea4bf12c15a
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 05:42:53 +0300

    Task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Genre ID for all shows
 SELECT tv_shows.title, tsg.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genre AS tsg
+LEFT JOIN tv_show_genres AS tsg
 ON tv_shows = tsg.show_id
 ORDER BY tv_shows.title, tsg.genre_id;
