5a2d80f1d59ae3c10af8507f384b25ab43fe4335
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 05:51:51 +0300

    Task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- No genre
 SELECT tvs.title, tvg.genre_id
 FROM tv_shows AS tvs
-LEFT JOIN tv_show_genres as tvg
+LEFT JOIN tv_show_genres AS tvg
 ON tvs.id = tvg.show_id
 WHERE tvg.genre_id IS NULL 
 ORDER BY tvs.title, tvg.genre_id;
