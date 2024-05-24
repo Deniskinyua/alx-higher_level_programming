f28bce6ca60855a9f1cc02d5347fe453088441c7
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-24 04:04:43 +0300

    Grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create user & grant all priveleges; If they not exists, script should not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd; GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
-FLUSH PRIVELEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
