dbaca3f961cb796e28c2100cca4bd27fff9178d4
Denis Kinyua <kimenjudenis@gmail.com>
2024-06-16 16:19:15 +0300

    Task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    printf("{} is zero".format(number))
+else:
+    printf("{} is negative".format(number))
